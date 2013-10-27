d5ef4e142eba687e304e2e64107d1f42b27461d0
cristoka7 <cristian.aceval@gmail.com>
2013-10-27 16:37:59 -0300

    Modificaciones en la vista Principal

--- Compiladores/src/com/py/compiladores/windows/Principal.java
+++ Compiladores/src/com/py/compiladores/windows/Principal.java
@@ -25,6 +25,8 @@ import java.awt.event.MouseEvent;
 import javax.swing.ButtonGroup;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
+import java.awt.Color;
 
 public class Principal {
 
@@ -59,20 +61,24 @@ public class Principal {
 	 */
 	private void initialize() {
 		frmTrabajoDeCompiladores = new JFrame();
-		frmTrabajoDeCompiladores.setTitle("Trabajo de Compiladores 2013");
+		frmTrabajoDeCompiladores.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 16));
+		frmTrabajoDeCompiladores.setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/com/sun/java/swing/plaf/motif/icons/DesktopIcon.gif")));
+		frmTrabajoDeCompiladores.setTitle(" Compiladores 2013");
 		frmTrabajoDeCompiladores.setBounds(100, 100, 487, 369);
 		frmTrabajoDeCompiladores.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTrabajoDeCompiladores.getContentPane().setLayout(null);
 		
 		JTextArea txtrTrabajoCompiladores = new JTextArea();
+		txtrTrabajoCompiladores.setBackground(new Color(60, 179, 113));
 		txtrTrabajoCompiladores.setFont(new Font("Monospaced", Font.BOLD, 16));
 		txtrTrabajoCompiladores.setEditable(false);
-		txtrTrabajoCompiladores.setBounds(118, 0, 269, 32);
+		txtrTrabajoCompiladores.setBounds(0, 0, 471, 32);
 		txtrTrabajoCompiladores.setTabSize(10);
-		txtrTrabajoCompiladores.setText("TRABAJO COMPILADORES 2013");
+		txtrTrabajoCompiladores.setText("                   COMPI-WIN");
 		frmTrabajoDeCompiladores.getContentPane().add(txtrTrabajoCompiladores);
 		
 		JButton btnPersonalizado = new JButton("Personalizado");
+		btnPersonalizado.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnPersonalizado.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -85,6 +91,7 @@ public class Principal {
 		frmTrabajoDeCompiladores.getContentPane().add(btnPersonalizado);
 		
 		JButton btnDefecto = new JButton("Predeterminado");
+		btnDefecto.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnDefecto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -98,16 +105,17 @@ public class Principal {
 		});
 		buttonGroup.add(btnDefecto);
 		btnDefecto.setBounds(148, 189, 207, 65);
-		btnDefecto.setIcon(new ImageIcon(Principal.class.getResource("/com/sun/java/swing/plaf/windows/icons/Computer.gif")));
+		btnDefecto.setIcon(new ImageIcon(Principal.class.getResource("/com/sun/java/swing/plaf/windows/icons/HardDrive.gif")));
 		frmTrabajoDeCompiladores.getContentPane().add(btnDefecto);
 		
 		JEditorPane dtrpnSeleccionDelAlfabeto = new JEditorPane();
-		dtrpnSeleccionDelAlfabeto.setFont(new Font("Tahoma", Font.BOLD, 12));
-		dtrpnSeleccionDelAlfabeto.setText("SELECCION DEL ALFABETO");
-		dtrpnSeleccionDelAlfabeto.setBounds(163, 71, 174, 20);
+		dtrpnSeleccionDelAlfabeto.setFont(new Font("Tahoma", Font.BOLD, 18));
+		dtrpnSeleccionDelAlfabeto.setText("          SELECCION DEL ALFABETO");
+		dtrpnSeleccionDelAlfabeto.setBounds(76, 59, 357, 32);
 		frmTrabajoDeCompiladores.getContentPane().add(dtrpnSeleccionDelAlfabeto);
 		
 		JEditorPane dtrpnCristianAceval = new JEditorPane();
+		dtrpnCristianAceval.setFont(new Font("Lucida Sans", Font.ITALIC, 14));
 		dtrpnCristianAceval.setText("Cristian Aceval - Victor Franco - Todos los derechos reservados");
 		dtrpnCristianAceval.setBounds(36, 299, 397, 20);
 		frmTrabajoDeCompiladores.getContentPane().add(dtrpnCristianAceval);
