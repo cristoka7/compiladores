0b2d88aeba0771fa30de695dbb5bb341e524275e
cristoka7 <cristian.aceval@gmail.com>
2013-10-28 10:08:55 -0300

    Modificaciones en la vista de Alfabeto Por defecto.. mejoras visuales

--- Compiladores/src/com/py/compiladores/windows/AlfabetoPorDefecto.java
+++ Compiladores/src/com/py/compiladores/windows/AlfabetoPorDefecto.java
@@ -17,6 +17,9 @@ import javax.swing.JButton;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JEditorPane;
 
 public class AlfabetoPorDefecto extends JFrame {
 
@@ -28,6 +31,8 @@ public class AlfabetoPorDefecto extends JFrame {
 	 * Create the frame.
 	 */
 	public AlfabetoPorDefecto() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(AlfabetoPorDefecto.class.getResource("/com/sun/java/swing/plaf/motif/icons/DesktopIcon.gif")));
 		setTitle("Alfabeto Predeterminado");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 480, 360);
@@ -37,27 +42,31 @@ public class AlfabetoPorDefecto extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("ALFABETOS PREDETERMINADOS");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel.setBounds(106, 11, 230, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(98, 51, 290, 24);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblDigitos = new JLabel("DIGITOS={0,1,2,3,4,5,6,7,8,9}");
-		lblDigitos.setBounds(24, 46, 174, 14);
+		lblDigitos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblDigitos.setBounds(24, 86, 312, 24);
 		contentPane.add(lblDigitos);
 		
 		JLabel lblLetrasabcdefghijklmnopqrstuvwxyz = new JLabel("LETRAS={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}");
-		lblLetrasabcdefghijklmnopqrstuvwxyz.setBounds(24, 82, 306, 14);
+		lblLetrasabcdefghijklmnopqrstuvwxyz.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblLetrasabcdefghijklmnopqrstuvwxyz.setBounds(24, 121, 394, 25);
 		contentPane.add(lblLetrasabcdefghijklmnopqrstuvwxyz);
 		
 		final TextField textField = new TextField();
-		textField.setBounds(135, 122, 240, 22);
+		textField.setBounds(154, 168, 300, 22);
 		contentPane.add(textField);
 		
-		Label label = new Label("Expresion Regular");
-		label.setBounds(22, 122, 107, 22);
+		Label label = new Label("Expresion Regular: ");
+		label.setFont(new Font("Tahoma", Font.BOLD, 12));
+		label.setBounds(24, 168, 126, 22);
 		contentPane.add(label);
 		
 		JButton btnProcesarEntrada = new JButton("Procesar Entrada");
+		btnProcesarEntrada.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnProcesarEntrada.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -84,7 +93,22 @@ public class AlfabetoPorDefecto extends JFrame {
 				}
 			}
 		});
-		btnProcesarEntrada.setBounds(137, 181, 152, 37);
+		btnProcesarEntrada.setBounds(154, 232, 182, 37);
 		contentPane.add(btnProcesarEntrada);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setText("                   COMPI-WIN");
+		textArea.setTabSize(10);
+		textArea.setFont(new Font("Monospaced", Font.BOLD, 16));
+		textArea.setEditable(false);
+		textArea.setBackground(new Color(60, 179, 113));
+		textArea.setBounds(0, 0, 473, 32);
+		contentPane.add(textArea);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setText("Cristian Aceval - Victor Franco - Todos los derechos reservados");
+		editorPane.setFont(new Font("Lucida Sans", Font.ITALIC, 12));
+		editorPane.setBounds(39, 290, 415, 20);
+		contentPane.add(editorPane);
 	}
 }
